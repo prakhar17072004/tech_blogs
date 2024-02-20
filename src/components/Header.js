628d080bd370b0ed7bdc6af48b05f2ed8138013f
prakhar17072004 <pmankur2004@gmail.com>
2024-02-20 16:27:27 +0530

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="py-4 border-b-2 border-b-gray-300 drop-shadow-md fixed top-0 inset-x-0 bg-white">
-      <h1 className="font-bold text-3xl uppercase text-center">Prakhar's Blogs</h1>
+      <h1 className="font-bold text-3xl uppercase text-center">Prakhar's Tech Blogs</h1>
     </div>
   )
 }
