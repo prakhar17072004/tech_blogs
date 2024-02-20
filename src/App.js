5ddcbdc14ed1681946389e6e63b7dca0d5ea9a9c
prakhar17072004 <pmankur2004@gmail.com>
2024-02-20 16:21:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect } from 'react'
+import Header from './components/Header';
+import Blogs from './components/Blogs'
+import Pagination from './components/Pagination';
+import { AppContext } from './context/Appcontext';
 
-function App() {
+
+export const App = () => {
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect(() => {
+    fetchBlogPosts();
+  }, [])
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full flex flex-col items-center justify-center gap-x-1">
+      <Header />
+      <Blogs />
+      <Pagination />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+ 
